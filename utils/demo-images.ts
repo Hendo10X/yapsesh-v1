f20ae4f77fe7bb4c7ad3e2a22e270a9af0646fa6
Hendo <hendersondike@gmail.com>
2025-04-23 19:13:54 +0100

    Add framer-motion and motion packages: Updated package.json and package-lock.json to include framer-motion and motion (version 12.8.0). Refactored Home component to use motion for animations and added Floating component for enhanced visual effects.

---
+++ utils/demo-images.ts
@@ -0,0 +1,10 @@
+export const exampleImages = [
+  { url: "https://picsum.photos/200/200" },
+  { url: "https://picsum.photos/200/300" },
+  { url: "https://picsum.photos/300/400" },
+  { url: "https://picsum.photos/250/250" },
+  { url: "https://picsum.photos/300/300" },
+  { url: "https://picsum.photos/400/500" },
+  { url: "https://picsum.photos/250/250" },
+  { url: "https://picsum.photos/300/400" },
+];
